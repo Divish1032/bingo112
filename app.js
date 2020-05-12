44d42afde815a5dd950b3884b1036043fc417452
Divish1032 <kumar.divansh1996@gmail.com>
2020-05-12 23:22:52 +0530

    Login UI

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post('/end3', function(req, res) {
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [0,1,2,3,4,5,6];
 rule.hour = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
-rule.minute = [15];
+rule.minute = [24,25,26,27,28];
 
 
  var j = schedule.scheduleJob(rule, function(){
