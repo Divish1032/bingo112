84c8b26d4c3cf98f07a48b6f683d0acd783673d7
Divish1032 <kumar.divansh1996@gmail.com>
2020-05-17 04:03:35 +0530

    initial

--- public/js/socket.js
+++ public/js/socket.js
@@ -83,7 +83,7 @@ socket.on('loadGameData', function(ticket, usedSequence){
     if(screen.width <= 550){
         $('.toast-message').text(screen.width)
         $('.toast').toast('show');
-        rotate(this);
+        rotate(window);
     }
     
 });
@@ -271,3 +271,45 @@ socket.on('error', (error) => {
 socket.on('reconnect', (attemptNumber) => {
     console.log("reconnected" + attemptNumber);
   });
+
+
+
+function fullScreenCheck() {
+    if (document.fullscreenElement) return;
+    return document.documentElement.requestFullscreen();
+}
+
+function updateDetails(lockButton) {
+    const buttonOrientation = getOppositeOrientation();
+}
+
+function getOppositeOrientation() {
+    const { type } = screen.orientation;
+    return type.startsWith("portrait") ? "landscape" : "portrait";
+}
+
+async function rotate(lockButton) {
+    try {
+        await fullScreenCheck();
+    } catch (err) {
+        console.error(err);
+}
+const newOrientation = getOppositeOrientation();
+    await screen.orientation.lock(newOrientation);
+    updateDetails(lockButton);
+}
+
+function show() {
+    const { type, angle } = screen.orientation;
+    console.log(`Orientation type is ${type} & angle is ${angle}.`);
+}
+
+screen.orientation.addEventListener("change", () => {
+    show();
+    updateDetails(document.getElementById("button"));
+});
+
+window.addEventListener("load", () => {
+    show();
+    updateDetails(document.getElementById("button"));
+});
